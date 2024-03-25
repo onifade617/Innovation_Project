6ea4716224a78dffc9dd9fe9b620f2c5c730567c
onifade617 <onifade617@gmail.com>
2024-03-25 11:17:08 +0100

    Update channels.py

--- channels.py
+++ channels.py
@@ -18,7 +18,7 @@ st.title("Application of Natural Language Processing Models on Political Informa
 penguins_df = pd.read_csv("CHANNELS_DATA.csv")
 
 penguins_df = penguins_df.drop(penguins_df.columns[0], axis=1)
-st.write(penguins_df)
+st.dataframe(penguins_df)
 selected_title = st.selectbox('Select a Title:', penguins_df['Title'].unique())
 
 # Get the content corresponding to the selected title
